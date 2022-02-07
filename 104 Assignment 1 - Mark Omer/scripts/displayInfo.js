bdda122c9f6d16cbc64e046b2e727205fdc1bbf7
markomer <97204116+markomer@users.noreply.github.com>
2022-02-07 12:44:31 -0700

    Add files via upload

---
+++ 104 Assignment 1 - Mark Omer/scripts/displayInfo.js
@@ -0,0 +1,17 @@
+function displayInformation(){
+  document.getElementById("salonInformation").innerHTML=`
+    <p> Welcome to ${salon.name} pet salon</p>
+    <p> You can find us at ${salon.address.street}, ${salon.address.number} </p>
+    <p> Open from ${salon.hours.open} to ${salon.hours.close}, Mon - Sat</p>
+  `;
+}
+displayInformation();
+
+/*function displayPetNames(){
+  document.getElementById("petNames").innerHTML=`
+
+  
+  
+  
+  `;
+}*/
